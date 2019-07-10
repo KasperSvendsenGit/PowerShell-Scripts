e8e3276d1fa4dae77c218e7b58ebe085ca3134fc
Kasper Svendsen <52733424+KasperSvendsenGit@users.noreply.github.com>
2019-07-10 08:45:17 +0200

    Create Get-SessionID-RDM.ps1

---
+++ Get-SessionID-RDM.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module "${env:ProgramFiles(x86)}\Devolutions\Remote Desktop Manager\RemoteDesktopManager.PowerShellModule.psd1"
+$DataSourceURL = "rdm://open?DataSource=8bce828e-a0e8-4b78-a12a-b16f44acd8be&Repository=&Session=" 
+
+$SessionHostName = Read-host -Prompt "Input The Exact Server Name CAPS SENSITIVE!"
+$GetSessionID = Get-RDMSession -Name "$SessionHostName" | Select-Object "ID"
+$RemovePrefix = "$GetSessionID".Substring(5)
+$Suffix = "$RemovePrefix".TrimEnd("}")
+$SessionURL = "$DataSourceURL$Suffix"
+Write-Output $SessionURL | clip
+echo "URL Copied to clipboard."
