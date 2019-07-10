351280dc5b07bff1153e1abd337240056b622f70
Kasper Svendsen <52733424+KasperSvendsenGit@users.noreply.github.com>
2019-07-10 10:48:54 +0200

    Spell check

--- Get-SessionID-RDM.ps1
+++ Get-SessionID-RDM.ps1
@@ -3,7 +3,7 @@ Import-Module "${env:ProgramFiles(x86)}\Devolutions\Remote Desktop Manager\Remot
 #	Example Data Soruce URL. This is unique for every datasource, you'll need to create your own the first time. 
 $DataSourceURL = "rdm://open?DataSource=8bce828e-a0e8-4b78-a12a-b16f44acd8be&Repository=&Session=" 
 #	Asks for Hostname of the session, input is CASE SENSITIVE and will break if there are dublicates.
-$SessionHostName = Read-host -Prompt "Input The Exact Server Name CASE SENSITIVE!"
+$SessionHostName = Read-host -Prompt "Input The Exact Session Name CASE SENSITIVE!"
 #	Gets the ID required to finish the URL from $DataSourceURL.
 $GetSessionID = Get-RDMSession -Name "$SessionHostName" | Select-Object "ID"
 #	The ID has an undesired prefix, this removes it. 
