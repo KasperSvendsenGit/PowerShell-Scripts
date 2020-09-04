add1a128530902df55372c58a2d457f8a54f4c67
Kasper Svendsen <52733424+KasperSvendsenGit@users.noreply.github.com>
2020-09-04 11:11:27 +0200

    Create Restart services.ps1

---
+++ Restart services.ps1
@@ -0,0 +1,58 @@
+﻿# Variables
+$rng = Get-Random -Maximum 10000000
+$dmdc_agent = Get-Service -Name "dmdc_agent" | Select-Object Name,DisplayName,Status
+$DMDC_SM = Get-Service -Name "DMDC_SM" | Select-Object Name,DisplayName,Status
+$DMDCCOMMUNICATE = Get-Service -Name "DMDCCOMMUNICATE" | Select-Object Name,DisplayName,Status
+$DMDCDATABASE = Get-Service -Name "DMDCDATABASE" | Select-Object Name,DisplayName,Status
+
+# Script start.
+Write-Host "Script starter..."
+timeout /T 3 > $env:temp\temp$rng.txt
+
+# Stop services.
+Write-Host "Stopper DMDC services..."
+Stop-Service -Name "dmdc_agent"
+Stop-Service -Name "DMDC_SM"
+Stop-Service -Name "DMDCCOMMUNICATE"
+Stop-Service -Name "DMDCDATABASE"
+
+# Test if services are running and start them if they're not.
+if($dmdc_agent.Status -eq "Stopped")
+    {Write-Host $dmdc_agent.DisplayName"køre ikke. Starter service..."; Start-Service -Name $dmdc_agent.Name}
+        elseif($dmdc_agent.Status -ne "Stopped"){Write-Host $dmdc_agent.DisplayName "køre allerede."}
+
+if($DMDC_SM.Status -eq "Stopped")
+    {Write-Host $DMDC_SM.DisplayName"køre ikke. Starter service..."; Start-Service -Name $DMDC_SM.Name}
+        elseif($DMDC_SM.Status -ne "Stopped"){Write-Host $DMDC_SM.DisplayName "køre allerede."}
+
+if($DMDC_SM.Status -eq "Stopped")
+    {Write-Host $DMDCCOMMUNICATE.DisplayName"køre ikke. Starter service..."; Start-Service -Name $DMDCCOMMUNICATE.Name}
+        elseif($DMDCCOMMUNICATE.Status -ne "Stopped"){Write-Host $DMDCCOMMUNICATE.DisplayName "køre allerede."}
+
+if($DMDCDATABASE.Status -eq "Stopped")
+    {Write-Host $DMDCDATABASE.DisplayName"køre ikke. Starter service..."; Start-Service -Name $DMDCDATABASE.Name}
+        elseif($DMDCDATABASE.Status -ne "Stopped"){Write-Host $DMDCDATABASE.DisplayName "køre allerede."}
+
+# Write status of services
+if($dmdc_agent.Status -eq "Running") {Write-Host $dmdc_agent.DisplayName"servicen køre..."}
+if($DMDC_SM.Status -eq "Running") {Write-Host $DMDC_SM.DisplayName"servicen køre..."}
+if($DMDCCOMMUNICATE.Status -eq "Running") {Write-Host $DMDCCOMMUNICATE.DisplayName"servicen køre..."}
+if($DMDCDATABASE.Status -eq "Running") {Write-Host $DMDCCOMMUNICATE.DisplayName"servicen køre..."}
+
+# Script end.
+Write-Host "Scriptet er færdig med at køre, du kan lukke vinduet nu..."
+pause
+
+
+
+
+
+
+
+
+
+
+
+
+
+
